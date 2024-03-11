eb43144287987f987f9c63fd1ee4ab17a2b3adf1
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-03-11 19:37:27 +0300

    Commit

---
+++ transactions.py
@@ -0,0 +1,25 @@
+# Account is empty (0) at the beginning of the year
+
+# Card fee is 5 ( 5 * 12 -> Yearly ) per month unless there were 3 payments made by card for a total cost of at least 100
+
+# Function solution takes 2 arguments, array A, transaction amounts and array D for transaction dates
+
+# Compute the final balance at the end of the year
+
+def solution ( A, D ) :
+    monthly_fee = 5
+    months = 12
+    # Total -> to hold the total balance  as the program loops
+    total = 0
+
+    for x in A :
+        total = total + x
+        print ( total )
+    
+    # Deducting the specified monthly fee from the total balance
+    grand_total = total - ( monthly_fee * months )
+    print ( grand_total )
+    return grand_total
+
+# Test run
+result = solution ([100, 100, 100, -10], ["2020-12-31","2020-12-22","2020-12-03", "2020-12-29"])
